0e3fa5cfc1f9727ee14e0ac22c17d0ce119268e7
Павел <pavel@imac-pavel.local>
2017-11-21 19:35:34 +0400

    Initial Commit

---
+++ FireFighters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireFighters
+//
+//  Created by Павел on 21.11.17.
+//  Copyright © 2017 Павел. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
